0cd13947e22bff010135ac87885437772334cd86
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-25 22:48:38 +0000

    Deploy shubich/pokemon-frontend-challenge to github.com/shubich/pokemon-frontend-challenge.git:gh-pages

---
+++ precache-manifest.318252f317fa6b04f7aaff9e50cbf628.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc1ebf2565e9b25388f532f904fbe2f8",
+    "url": "/pokemon-frontend-challenge/index.html"
+  },
+  {
+    "revision": "f1f14dc8ccdd8a06a736",
+    "url": "/pokemon-frontend-challenge/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "cfd0d1edda44e7f062bd",
+    "url": "/pokemon-frontend-challenge/static/css/main.4d1c4fa3.chunk.css"
+  },
+  {
+    "revision": "f1f14dc8ccdd8a06a736",
+    "url": "/pokemon-frontend-challenge/static/js/2.f32356a6.chunk.js"
+  },
+  {
+    "revision": "cfd0d1edda44e7f062bd",
+    "url": "/pokemon-frontend-challenge/static/js/main.bde28383.chunk.js"
+  },
+  {
+    "revision": "d9fb5c81fd9d6b4c820c",
+    "url": "/pokemon-frontend-challenge/static/js/runtime~main.ac307507.js"
+  }
+]);
